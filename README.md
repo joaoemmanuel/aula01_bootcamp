0c19bfefb807337625eb3699018c590edb8e1551
joaoemmanuel <joao.basass@gmail.com>
2025-01-27 18:05:13 -0300

    Atualizei o README com instruções para execução do arquivo kpi.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Aula 01 - Bootcamp
+
+## Como preencher as informações para execução do kpi.py:
+
+1) Preencher o nome;
+2) Preencher o salário (usar . ao invés de vírgula)
+3) Preencher o bônus (usar . ao invés de vírgula)
--- kpi.py
+++ kpi.py
@@ -2,9 +2,11 @@
 #Escreva um programa em Python que solicita ao usuário para digitar seu nome, 
 #o valor do seu salário mensal e o valor do bônus que recebeu. 
 #O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome e informando o valor do salário em comparação com o bônus recebido.
+#O cálculo do KPI do bônus é 1000 + Salário * Bônus
 
 nome  = input("Informe o seu nome: ")
 sal   = input("Informe o seu salário mensal: ")
 bonus = input("Informe o seu bônus recebido: ")
+final = 1000 + (float(sal) * float(bonus))
 
-print(f"Olá {nome}!\n Seu salário atual é de {sal} e ")
+print(f"Olá {nome}, seu bônus foi de {final}")
